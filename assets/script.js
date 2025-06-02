e028936971190e5e915e0f72a915d80ee770533a
ThNs7 <thiagoo.nasci77@gmail.com>
2025-06-02 18:33:57 -0300

    feat: Header animation.

---
+++ assets/script.js
@@ -0,0 +1,27 @@
+document.addEventListener("DOMContentLoaded", function () {
+  
+  const header = document.querySelector('header');
+
+
+  if (header) {
+    setTimeout(() => {
+      header.classList.add('appear');
+    }, 200);
+  }
+
+  
+  const faixa = document.querySelector('.slide-faixa');
+  const slides = document.querySelectorAll('.slide');
+
+  if (faixa && slides.length > 0) {
+    let currentIndex = 0;
+
+    function mostrarSlide() {
+      const offset = -currentIndex * 100;
+      faixa.style.transform = `translateX(${offset}%)`;
+      currentIndex = (currentIndex + 1) % slides.length;
+    }
+
+    setInterval(mostrarSlide, 4000);
+  }
+});
