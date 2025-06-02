1cf1ea8f9ec453ba54d475f5b390b3fe9caa0146
ThNs7 <thiagoo.nasci77@gmail.com>
2025-06-02 13:41:05 -0300

    feat: Publico Alvo.

---
+++ assets/publico.js
@@ -0,0 +1,13 @@
+document.addEventListener("DOMContentLoaded", function () {
+  const faixa = document.querySelector('.slide-faixa');
+  const totalSlides = document.querySelectorAll('.slide').length;
+  let currentIndex = 0;
+
+  function mostrarSlide() {
+    const offset = -currentIndex * 100;
+    faixa.style.transform = `translateX(${offset}%)`;
+    currentIndex = (currentIndex + 1) % totalSlides;
+  }
+
+  setInterval(mostrarSlide, 5000);
+});
